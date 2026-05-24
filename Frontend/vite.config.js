2a1016a689fffadb349e172b942e5c1d38c50380
Shefali Yadav <Shefaliyadav1709@gmail.com>
2026-05-24 11:10:16 +0530

    backend deployed changes on frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server:{
   proxy:{
     "/api":{
-      target:"http://localhost:3000",
+      target:"https://your-backend-url.onrender.com",
       changeOrigin:true,
       secure:false,
     }
